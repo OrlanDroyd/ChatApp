48b2c8bc6310b8af4f28a61e3aa6654a5600170b
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-06-09 12:27:44 -0400

    MainActivity.kt modified

--- app/src/main/java/com/gmail/orlandroyd/chatapp/MainActivity.kt
+++ app/src/main/java/com/gmail/orlandroyd/chatapp/MainActivity.kt
@@ -3,39 +3,41 @@ package com.gmail.orlandroyd.chatapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.gmail.orlandroyd.chatapp.ui.theme.ChatAppTheme
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.gmail.orlandroyd.chatapp.presentation.chat.ChatScreen
+import com.gmail.orlandroyd.chatapp.presentation.username.UsernameScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChatAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "username_screen"
+            ) {
+                composable("username_screen") {
+                    UsernameScreen(onNavigate = navController::navigate)
+                }
+                composable(
+                    route = "chat_screen/{username}",
+                    arguments = listOf(
+                        navArgument(name = "username") {
+                            type = NavType.StringType
+                            nullable = true
+                        }
+                    )
+                ) {
+                    val username = it.arguments?.getString("username")
+                    ChatScreen(username = username)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ChatAppTheme {
-        Greeting("Android")
     }
 }
