cdec6dc7b88b640e6f3884f09d974a9f46eb3ff7
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-06-09 10:59:46 -0400

    MessageDto.kt modified create mapper

--- app/src/main/java/com/gmail/orlandroyd/chatapp/data/remote/dto/MessageDto.kt
+++ app/src/main/java/com/gmail/orlandroyd/chatapp/data/remote/dto/MessageDto.kt
@@ -1,6 +1,9 @@
 package com.gmail.orlandroyd.chatapp.data.remote.dto
 
+import com.gmail.orlandroyd.chatapp.domain.model.Message
 import kotlinx.serialization.Serializable
+import java.text.DateFormat
+import java.util.*
 
 @Serializable
 data class MessageDto(
@@ -8,4 +11,16 @@ data class MessageDto(
     val timestamp: Long,
     val username: String,
     val id: String,
-)
+) {
+    fun toMessage(): Message {
+        val date = Date(timestamp)
+        val formattedDate = DateFormat
+            .getDateInstance(DateFormat.DEFAULT)
+            .format(date)
+        return Message(
+            text = text,
+            formattedTime = formattedDate,
+            username = username
+        )
+    }
+}
